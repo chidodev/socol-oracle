c4f2a5919a57d6080aaf7caeb257424e3c2cadcf
chidodev <chidev0104@gmail.com>
2022-09-12 22:03:20 +0800

    feat: hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,44 @@
+
+import 'dotenv/config';
+import 'hardhat-gas-reporter';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+let accounts;
+if (process.env.ACCOUNT_PRIVATE_KEY) {
+  accounts = [process.env.ACCOUNT_PRIVATE_KEY];
+}
+
 const config: HardhatUserConfig = {
+  gasReporter: {
+    enabled: true,
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+  },
+
   solidity: "0.8.9",
 };
 
